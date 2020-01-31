6cfffd3f7515053456fcc8966212f5b9c21c2bc5
Daragh Carroll <dcarroll@fexco.ie@ftsliu0127c.fexco.ie>
2020-01-31 09:25:02 +0000

    Added PintsBot

---
+++ Pints-Bot/index.js
@@ -0,0 +1,49 @@
+const request = require('request-promise');
+
+const hook = 'TTB5LAL8M/BTDBCNJCX/1bqaK4Kvz1TCPsEcvWk0Ii98';
+
+const getData = async function()
+{
+    const json = await request( { 
+        url: 'https://next.json-generator.com/api/json/get/Eys6SRn-_',
+        json: true
+    });
+    return json.map(person => ({
+        age: person.age,
+        email: person.email,
+        firstName: person.name.first,
+        lastName: person.name.last
+
+    }))
+};
+
+(async function () {
+    try{
+        const people = await getData();
+        
+        const slackBody = {
+            mkdwn: true,
+            test: `Pints, Pints!, PINTS!`,
+            attachments: people.map(person => ({
+                color: 'good',
+                text: `*${person.firstName} ${person.lastName}* is looking to go for pints. Contact them at ${person.email}`,
+            }))
+        };
+
+        //slackpost
+        const res = await request({
+            url: `https://hooks.slack.com/services/${hook}`,
+            method: 'POST',
+            body: slackBody,
+            json : true
+        });
+
+        console.log(res);
+
+    } catch(e)
+    {
+        console.log('error', e);
+    }
+
+    debugger;
+})();
